707a49570d09f67b7be20abacf897f03ab65145d
。 <chinafxw@126.com>
2021-11-29 18:32:39 +0800

    修改了testGit中的内容

--- src/com/cowain/TestGit.java
+++ src/com/cowain/TestGit.java
@@ -1,7 +1,45 @@
 package com.cowain;
 
+import java.io.Serializable;
+
 /**
  * @author: fxw
  */
-public class TestGit {
+public class TestGit implements Serializable {
+    private String name;
+    private int age;
+    private  String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public TestGit() {
+    }
+
+    public TestGit(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
